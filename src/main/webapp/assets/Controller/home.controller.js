a60db54ffe07a5a9844dda85ca2e235bb6910704
nitin <nitin@example.com>
2016-02-18 19:24:01 +0530

    about us

--- src/main/webapp/assets/Controller/home.controller.js
+++ src/main/webapp/assets/Controller/home.controller.js
@@ -5,11 +5,46 @@
 		.module('app')
 		.controller('HomeController', HomeController);
  
-	HomeController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-	function HomeController($location, AuthenticationService, FlashService) {
+	HomeController.$inject = ['$location','$scope','pageInfo', 'AuthenticationService', 'FlashService'];
+	/*function HomeController($location, AuthenticationService, FlashService) {
 		var vm = this;
  
 		
+	}*/
+	function HomeController($location,$scope,pageInfo){
+  		$scope.myInterval = 3000;
+
+  		pageInfo.name = '';
+  		/*$scope.getClassNitin = function (path) {
+		  if ($location.path().substr(0, path.length) === path) {
+		    return 'active';
+		  } else {
+		    return '';
+		  }
+		}*/
+  		/*$scope.slides = [{
+				image: 'http://lorempixel.com/400/200/'
+			},
+			{
+				image: 'http://lorempixel.com/400/200/food'
+			},
+			{
+				image: 'http://lorempixel.com/400/200/sports'
+			},
+			{
+				image: 'http://lorempixel.com/400/200/people'
+			}
+		];*/
 	}
- 
+	/*angular.module("app").directive("activeTab",[function(){
+		return{
+			restrict:"E",
+			scope:{},
+			link:function(scope,element,attrs){
+				alert("we")
+			}
+		}
+
+	}]);*/
+
 })();
